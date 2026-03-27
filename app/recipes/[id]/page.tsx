6ae2065e8fb5d00f8382a604c0c99a7f31606a99
loic-v-noven <loic@noven.be>
2026-03-27 19:50:14 +0100

    Force dynamic rendering on all DB pages to fix Vercel build

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { db } from "@/lib/db";
 import { recipes, ingredients } from "@/lib/schema";
 import { eq, asc } from "drizzle-orm";
--- app/recipes/new/page.tsx
+++ app/recipes/new/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import RecipeForm from "@/components/recipe-form";
 import { getAllTags } from "@/actions/recipes";
 
